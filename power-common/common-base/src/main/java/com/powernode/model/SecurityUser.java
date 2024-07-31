0adc0887344cb301609bce8c0f99672f17250894
G-WS <3355139577@qq.com>
2024-07-31 22:00:34 +0800

    根据标识查询角色详情

--- power-common/common-base/src/main/java/com/powernode/model/SecurityUser.java
+++ power-common/common-base/src/main/java/com/powernode/model/SecurityUser.java
@@ -1,7 +1,5 @@
 package com.powernode.model;
 
-import cn.hutool.core.lang.hash.Hash;
-import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,10 +14,10 @@ import java.util.Set;
  * Security安全框架认识的安全用户对象
  */
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class SecurityUser implements UserDetails {
-    //商城后台管理系统用户的相关属性
+    // 商城后台管理系统用户的相关属性
     private Long userId;
     private String username;
     private String password;
@@ -27,7 +25,9 @@ public class SecurityUser implements UserDetails {
     private Long shopId;
     private String loginType;
     private Set<String> perms = new HashSet<>();
-    //商城购物系统会员的相关属性
+
+    // 商城购物系统会员的相关属性
+    private String openid;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -41,32 +41,12 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return this.loginType + this.userId;
+        return this.loginType+this.userId;
     }
 
     @Override
     public boolean isAccountNonExpired() {
         return this.status == 1;
-    }
-
-    public Set<String> getPerms() {
-        HashSet<String> finalPermsSet = new HashSet<>();
-        //遍历用户权限集合
-        perms.forEach(perm -> {
-            if (perms.contains(",")) {
-                //包含，说明一条权限里面有多个权限
-                //根据，号进行分隔处理
-                String[] realPerms = perm.split(",");
-                for (String realPerm : realPerms) {
-                    finalPermsSet.add(realPerm);
-                }
-
-            } else {
-                //不包含即只有一条权限
-                finalPermsSet.add(perm);
-            }
-        });
-        return finalPermsSet;
     }
 
     @Override
@@ -82,5 +62,26 @@ public class SecurityUser implements UserDetails {
     @Override
     public boolean isEnabled() {
         return this.status == 1;
+    }
+
+    public Set<String> getPerms() {
+        HashSet<String> finalPermsSet = new HashSet<>();
+        // 循环遍历用户权限集合
+        perms.forEach(perm -> {
+            // 判断是否包含,
+            if (perm.contains(",")) {
+                // 包含：说明一条权限里有多个权限
+                // 根据,号进行分隔处理
+                String[] realPerms = perm.split(",");
+                // 循环遍历
+                for (String realPerm : realPerms) {
+                    finalPermsSet.add(realPerm);
+                }
+            } else {
+                // 不包含，即一条权限
+                finalPermsSet.add(perm);
+            }
+        });
+        return finalPermsSet;
     }
 }
