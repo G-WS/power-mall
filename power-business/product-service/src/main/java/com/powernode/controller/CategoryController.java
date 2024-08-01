796ac58dbe633fd45cb40c745cb169468febfcc8
G-WS <3355139577@qq.com>
2024-08-01 11:56:17 +0800

    增删查改商品所有类目

--- power-business/product-service/src/main/java/com/powernode/controller/CategoryController.java
+++ power-business/product-service/src/main/java/com/powernode/controller/CategoryController.java
@@ -34,72 +34,72 @@ public class CategoryController {
         List<Category> list = categoryService.queryAllCategoryList();
         return Result.success(list);
     }
-//
-//    /**
-//     * 查询系统商品一级类目
-//     * @return
-//     */
-////    prod/category/listCategory
-//    @ApiOperation("查询系统商品一级类目")
-//    @GetMapping("listCategory")
-//    @PreAuthorize("hasAuthority('prod:category:page')")
-//    public Result<List<Category>> loadFirstCategoryList() {
-//        List<Category> list = categoryService.queryFirstCategoryList();
-//        return Result.success(list);
-//    }
-//
-//    /**
-//     * 新增商品类目
-//     * @param category 商品类目对象
-//     * @return
-//     */
-//    @ApiOperation("新增商品类目")
-//    @PostMapping
-//    @PreAuthorize("hasAuthority('prod:category:save')")
-//    public Result<String> saveCategory(@RequestBody Category category) {
-//        Boolean saved = categoryService.saveCategory(category);
-//        return Result.handle(saved);
-//    }
-//
-//    /**
-//     * 根据标识查询商品类目详情
-//     * @param categoryId 商品类目标识
-//     * @return
-//     */
-//    @ApiOperation("根据标识查询商品类目详情")
-//    @GetMapping("info/{categoryId}")
-//    @PreAuthorize("hasAuthority('prod:category:info')")
-//    public Result<Category> loadCategoryInfo(@PathVariable Long categoryId) {
-//        Category category = categoryService.getById(categoryId);
-//        return Result.success(category);
-//    }
-//
-//    /**
-//     * 修改商品类目信息
-//     * @param category 商品类目对象
-//     * @return
-//     */
-//    @ApiOperation("修改商品类目信息")
-//    @PutMapping
-//    @PreAuthorize("hasAuthority('prod:category:update')")
-//    public Result<String> modifyCategory(@RequestBody Category category) {
-//        Boolean modified = categoryService.modifyCategory(category);
-//        return Result.handle(modified);
-//    }
-//
-//    /**
-//     * 删除商品类目
-//     * @param categoryId 商品类目标识
-//     * @return
-//     */
-//    @ApiOperation("删除商品类目")
-//    @DeleteMapping("{categoryId}")
-//    @PreAuthorize("hasAuthority('prod:category:delete')")
-//    public Result<String> removeCategory(@PathVariable Long categoryId) {
-//        Boolean removed = categoryService.removeCategoryById(categoryId);
-//        return Result.handle(removed);
-//    }
-//
+
+    /**
+     * 查询系统商品一级类目
+     * @return
+     */
+//    prod/category/listCategory
+    @ApiOperation("查询系统商品一级类目")
+    @GetMapping("listCategory")
+    @PreAuthorize("hasAuthority('prod:category:page')")
+    public Result<List<Category>> loadFirstCategoryList() {
+        List<Category> list = categoryService.queryFirstCategoryList();
+        return Result.success(list);
+    }
+
+    /**
+     * 新增商品类目
+     * @param category 商品类目对象
+     * @return
+     */
+    @ApiOperation("新增商品类目")
+    @PostMapping
+    @PreAuthorize("hasAuthority('prod:category:save')")
+    public Result<String> saveCategory(@RequestBody Category category) {
+        Boolean saved = categoryService.saveCategory(category);
+        return Result.handle(saved);
+    }
+
+    /**
+     * 根据标识查询商品类目详情
+     * @param categoryId 商品类目标识
+     * @return
+     */
+    @ApiOperation("根据标识查询商品类目详情")
+    @GetMapping("info/{categoryId}")
+    @PreAuthorize("hasAuthority('prod:category:info')")
+    public Result<Category> loadCategoryInfo(@PathVariable Long categoryId) {
+        Category category = categoryService.getById(categoryId);
+        return Result.success(category);
+    }
+
+    /**
+     * 修改商品类目信息
+     * @param category 商品类目对象
+     * @return
+     */
+    @ApiOperation("修改商品类目信息")
+    @PutMapping
+    @PreAuthorize("hasAuthority('prod:category:update')")
+    public Result<String> modifyCategory(@RequestBody Category category) {
+        Boolean modified = categoryService.modifyCategory(category);
+        return Result.handle(modified);
+    }
+
+    /**
+     * 删除商品类目
+     * @param categoryId 商品类目标识
+     * @return
+     */
+    @ApiOperation("删除商品类目")
+    @DeleteMapping("{categoryId}")
+    @PreAuthorize("hasAuthority('prod:category:delete')")
+    public Result<String> removeCategory(@PathVariable Long categoryId) {
+        Boolean removed = categoryService.removeCategoryById(categoryId);
+        return Result.handle(removed);
+    }
+
 //    ////////////////// 微信小程序数据接口 //////////////////////////
 //
 //    /**
--- power-business/product-service/src/main/java/com/powernode/service/impl/CategoryServiceImpl.java
+++ power-business/product-service/src/main/java/com/powernode/service/impl/CategoryServiceImpl.java
@@ -32,95 +32,97 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
         );
     }
 
-//    @Override
-//    @Cacheable(key = ProductConstants.FIRST_CATEGORY_LIST_KEY)
-//    public List<Category> queryFirstCategoryList() {
-//        return categoryMapper.selectList(new LambdaQueryWrapper<Category>()
-//                .eq(Category::getParentId,0)
-//                .eq(Category::getStatus,1)
-//                .orderByDesc(Category::getSeq)
-//        );
-//    }
-//
-//    @Override
-//    @Caching(evict = {
-//            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
-//    })
-//    public Boolean saveCategory(Category category) {
-//        category.setCreateTime(new Date());
-//        category.setUpdateTime(new Date());
-//        return categoryMapper.insert(category)>0;
-//    }
-//
-//    /**
-//     * 修改商品类目
-//     *  需求：允许商品类目修改类目级别
-//     * @param category
-//     * @return
-//     */
-//    @Override
-//    @Caching(evict = {
-//            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
-//    })
-//    public Boolean modifyCategory(Category category) {
-//        // 修改后的pid
-//        Long parentId = category.getParentId();
-//        // 根据标识查询类目详情
-//        Category beforeCategory = categoryMapper.selectById(category.getCategoryId());
-//        // 获取商品类目之前的级别,如果parentId为0即为1级类目，不为0即为2级类目
-//        Long beforeParentId = beforeCategory.getParentId();
-//        // 判断商品类目修改的详情
-//        // 1 -> 2 : 之前pid为0 且 修改后的pid不为0
-//        if (0 == beforeParentId && null != parentId && 0 != parentId) {
-//            // 查询当前类目是否包含子类目，如果包含子类目，则不允许修改
-//            // 根据当前类目标识查询子类目
-//            List<Category> childList = categoryMapper.selectList(new LambdaQueryWrapper<Category>()
-//                    .eq(Category::getParentId, category.getCategoryId())
-//            );
-//            // 判断是否有值
-//            if (CollectionUtil.isNotEmpty(childList) && childList.size() != 0) {
-//                // 说明：当前类目包含子类目，不允许修改
-//                throw new BusinessException("当前类目包含子类目，不可修改");
-//            }
-//        }
-//
-//        // 2 -> 1：之前pid不为0 且 当前pid为null
-//        if (0 != beforeParentId && null == parentId) {
-//            category.setParentId(0L);
-//        }
-//        return categoryMapper.updateById(category)>0;
-//    }
-//
-//    /**
-//     * 删除商品类目
-//     *  需求：如果一级类目包含子类目，则不可删除
-//     * @param categoryId
-//     * @return
-//     */
-//    @Override
-//    @Caching(evict = {
-//            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
-//            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
-//    })
-//    public Boolean removeCategoryById(Long categoryId) {
-//        // 根据类目标识查询子类目集合
-//        List<Category> childCategoryList = categoryMapper.selectList(new LambdaQueryWrapper<Category>()
-//                .eq(Category::getParentId, categoryId)
-//        );
-//        // 判断是否有值
-//        if (CollectionUtil.isNotEmpty(childCategoryList) && childCategoryList.size() != 0) {
-//            // 说明：当前类目包含子类目，不可删除
-//            throw new BusinessException("当前类目包含子类目，不可删除");
-//        }
-//        // 说明：当前类目不包含子类目
-//        return categoryMapper.deleteById(categoryId)>0;
-//    }
-//
+    @Override
+    @Cacheable(key = ProductConstants.FIRST_CATEGORY_LIST_KEY)
+    public List<Category> queryFirstCategoryList() {
+        return categoryMapper.selectList(new LambdaQueryWrapper<Category>()
+                .eq(Category::getParentId,0)
+                .eq(Category::getStatus,1)
+                .orderByDesc(Category::getSeq)
+        );
+    }
+
+    //符合缓存清楚
+    @Override
+    @Caching(evict = {
+            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
+    })
+    public Boolean saveCategory(Category category) {
+        category.setCreateTime(new Date());
+        category.setUpdateTime(new Date());
+        return categoryMapper.insert(category)>0;
+    }
+
+    /**
+     * 修改商品类目
+     *  需求：允许商品类目修改类目级别
+     * @param category
+     * @return
+     */
+    @Override
+    @Caching(evict = {
+            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
+    })
+    public Boolean modifyCategory(Category category) {
+        // 修改后的pid
+        Long parentId = category.getParentId();
+        // 根据标识查询类目详情
+        Category beforeCategory = categoryMapper.selectById(category.getCategoryId());
+        // 获取商品类目之前的级别,如果parentId为0即为1级类目，不为0即为2级类目
+        Long beforeParentId = beforeCategory.getParentId();
+        // 判断商品类目修改的详情
+        // 1 -> 2 : 之前pid为0 且 修改后的pid不为0
+        if (0 == beforeParentId && null != parentId && 0 != parentId) {
+            // 查询当前类目是否包含子类目，如果包含子类目，则不允许修改
+            // 根据当前类目标识查询子类目
+            List<Category> childList = categoryMapper.selectList(new LambdaQueryWrapper<Category>()
+                    .eq(Category::getParentId, category.getCategoryId())
+            );
+            // 判断是否有值
+            if (CollectionUtil.isNotEmpty(childList) && childList.size() != 0) {
+                //当商品类目进行修改的时候，不能出现其存在子类目的情况
+                // 说明：当前类目包含子类目，不允许修改
+                throw new BusinessException("当前类目包含子类目，不可修改");
+            }
+        }
+
+        // 2 -> 1：之前pid不为0 且 当前pid为null
+        if (0 != beforeParentId && null == parentId) {
+            category.setParentId(0L);
+        }
+        return categoryMapper.updateById(category)>0;
+    }
+
+    /**
+     * 删除商品类目
+     *  需求：如果一级类目包含子类目，则不可删除
+     * @param categoryId
+     * @return
+     */
+    @Override
+    @Caching(evict = {
+            @CacheEvict(key = ProductConstants.ALL_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.FIRST_CATEGORY_LIST_KEY),
+            @CacheEvict(key = ProductConstants.WX_FIRST_CATEGORY)
+    })
+    public Boolean removeCategoryById(Long categoryId) {
+        // 根据类目标识查询子类目集合
+        List<Category> childCategoryList = categoryMapper.selectList(new LambdaQueryWrapper<Category>()
+                .eq(Category::getParentId, categoryId)
+        );
+        // 判断是否有值
+        if (CollectionUtil.isNotEmpty(childCategoryList) && childCategoryList.size() != 0) {
+            // 说明：当前类目包含子类目，不可删除
+            throw new BusinessException("当前类目包含子类目，不可删除");
+        }
+        // 说明：当前类目不包含子类目
+        return categoryMapper.deleteById(categoryId)>0;
+    }
+
 //    @Override
 //    @Cacheable(key = ProductConstants.WX_FIRST_CATEGORY)
 //    public List<Category> queryWxCategoryListByPid(Long pid) {
