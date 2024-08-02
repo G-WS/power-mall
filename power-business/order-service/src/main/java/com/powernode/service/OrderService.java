6ff2b4b95ae5b331abc704fa789f65ddcfe6a177
G-WS <3355139577@qq.com>
2024-08-02 21:55:28 +0800

    订单增删查改

--- power-business/order-service/src/main/java/com/powernode/service/OrderService.java
+++ power-business/order-service/src/main/java/com/powernode/service/OrderService.java
@@ -3,6 +3,10 @@ package com.powernode.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.powernode.domain.Order;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.powernode.model.ChangeStock;
+import com.powernode.model.OrderConfirmParam;
+import com.powernode.vo.OrderStatusCount;
+import com.powernode.vo.OrderVo;
 
 import java.util.Date;
 
@@ -27,60 +31,60 @@ public interface OrderService extends IService<Order>{
      */
     Order queryOrderDetailByOrderNumber(Long orderNumber);
 
-//    /**
-//     * 查询会员订单各状态数量
-//     * @return
-//     */
-//    OrderStatusCount queryMemberOrderStatusCount();
-//
-//    /**
-//     * 分页查询会员订单列表
-//     * @param current
-//     * @param size
-//     * @param status
-//     * @return
-//     */
-//    Page<Order> queryMemberOrderPage(Long current, Long size, Long status);
-//
-//    /**
-//     * 根据订单编号查询订单详情
-//     * @param orderNumber
-//     * @return
-//     */
-//    Order queryMemberOrderDetailByOrderNumber(String orderNumber);
-//
-//    /**
-//     * 会员确认收货
-//     * @param orderNumber
-//     * @return
-//     */
-//    Boolean receiptMemberOrder(String orderNumber);
-//
-//    /**
-//     * 删除会员订单
-//     * @param orderNumber
-//     * @return
-//     */
-//    Boolean removeMemberOrderByOrderNumber(String orderNumber);
-//
-//    /**
-//     * 查询会员确认订单页面数据
-//     * @param orderConfirmParam
-//     * @return
-//     */
-//    OrderVo queryMemberOrderConfirmVo(OrderConfirmParam orderConfirmParam);
-//
-//    /**
-//     * 会员提交订单
-//     * @param orderVo
-//     * @return
-//     */
-//    String submitOrder(OrderVo orderVo);
-//
-//    /**
-//     * 订单回滚
-//     * @param order
-//     * @param changeStock
-//     */
-//    void orderRollBack(Order order, ChangeStock changeStock);
+    /**
+     * 查询会员订单各状态数量
+     * @return
+     */
+    OrderStatusCount queryMemberOrderStatusCount();
+
+    /**
+     * 分页查询会员订单列表
+     * @param current
+     * @param size
+     * @param status
+     * @return
+     */
+    Page<Order> queryMemberOrderPage(Long current, Long size, Long status);
+
+    /**
+     * 根据订单编号查询订单详情
+     * @param orderNumber
+     * @return
+     */
+    Order queryMemberOrderDetailByOrderNumber(String orderNumber);
+
+    /**
+     * 会员确认收货
+     * @param orderNumber
+     * @return
+     */
+    Boolean receiptMemberOrder(String orderNumber);
+
+    /**
+     * 删除会员订单
+     * @param orderNumber
+     * @return
+     */
+    Boolean removeMemberOrderByOrderNumber(String orderNumber);
+
+    /**
+     * 查询会员确认订单页面数据
+     * @param orderConfirmParam
+     * @return
+     */
+    OrderVo queryMemberOrderConfirmVo(OrderConfirmParam orderConfirmParam);
+
+    /**
+     * 会员提交订单
+     * @param orderVo
+     * @return
+     */
+    String submitOrder(OrderVo orderVo);
+
+    /**
+     * 订单回滚
+     * @param order
+     * @param changeStock
+     */
+    void orderRollBack(Order order, ChangeStock changeStock);
 }
