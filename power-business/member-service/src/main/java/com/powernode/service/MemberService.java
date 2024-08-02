434566fb724f5d2d3ef171e60b27b81b2eabc0b9
G-WS <3355139577@qq.com>
2024-08-02 21:10:58 +0800

    更新会员的头像和id

--- power-business/member-service/src/main/java/com/powernode/service/MemberService.java
+++ power-business/member-service/src/main/java/com/powernode/service/MemberService.java
@@ -5,4 +5,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface MemberService extends IService<Member>{
 
 
+    /**
+     * 更新会员的头像和昵称
+     * @param member
+     * @return
+     */
+    Boolean modifyMemberInfoByOpenId(Member member);
 }
