af32187b08ed4dcf653f2b797002eaec1b4ff51e
G-WS <3355139577@qq.com>
2024-08-02 11:46:27 +0800

    查询全国地区列表

--- power-business/store-service/src/main/java/com/powernode/service/AreaService.java
+++ power-business/store-service/src/main/java/com/powernode/service/AreaService.java
@@ -2,7 +2,16 @@ package com.powernode.service;
 
 import com.powernode.domain.Area;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
 public interface AreaService extends IService<Area>{
 
+
+    /**
+     * 查询全国地区列表
+     * @return
+     */
+    List<Area> queryAllAreaList();
 
 }
